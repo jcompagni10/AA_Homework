cbbd8b8105fd6b63de8a5f61e4796ce69a1d2d19
Julian Compagni Portis <jcompagni@gmail.com>
2017-10-08 15:03:16 -0700

    simon game

---
+++ W2D1/skeleton/lib/simon.rb
@@ -0,0 +1,82 @@
+require "byebug"
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq= []
+
+  end
+
+  def play
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+    if !@game_over
+      round_success_message
+      @sequence_length += 1
+    end
+  end
+
+  def seq_to_s
+    @seq.join(' ')
+  end
+
+  def show_sequence
+    system("clear")
+    add_random_color
+    puts "Simon Says: #{seq_to_s}"
+    sleep(@sequence_length * 0.5)
+    system("clear")
+  end
+
+  def require_sequence
+    puts "Your Turn"
+    idx = 0
+    until (idx == sequence_length) || @game_over
+      color = prompt_color(idx)
+      @game_over = color != @seq[idx]
+      idx += 1
+    end
+  end
+
+  def prompt_color(n)
+    print "#{n}: "
+    gets.chomp
+  end
+
+  def add_random_color
+    @seq << COLORS[rand(4)]
+
+  end
+
+  def round_success_message
+    puts "CORRECT"
+    sleep(1)
+
+  end
+
+  def game_over_message
+    puts "GAME OVER!!"
+    puts "Score: #{@sequence_length}"
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq= []
+  end
+end
+
+g= Simon.new
+g.play
