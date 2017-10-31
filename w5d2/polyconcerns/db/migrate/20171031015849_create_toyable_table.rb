ba1aec28558552a498495e1bb6734c1e4714bde5
Julian Compagni Portis <jcompagni@gmail.com>
2017-10-30 19:43:04 -0700

    hw w5dw

---
+++ w5d2/polyconcerns/db/migrate/20171031015849_create_toyable_table.rb
@@ -0,0 +1,9 @@
+class CreateToyableTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :toys do |t|
+      t.string :name, null: false, index: true, unique: true
+      t.references :toyable, polymorphic: true, index: true
+    end
+    add_index :toys, [:name, :toyable_id, :toyable_type], unique: true
+  end
+end
